5177178586e7934e0a43e618730a9c75cb06b822
Alex Au-Yeung <ah.auyeung@gmail.com>
2021-12-02 11:06:51 +1100

    header setup

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,28 @@ import 'bootstrap/dist/css/bootstrap.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="app-header bg-dark text-white">
         <div className="container">
-        <img src={logo} className="App-logo" alt="logo" />
+          <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a href="/" className="d-flex align-items-center text-decoration-none">
+                <img src={logo} className="App-logo" alt="logo" />
+            </a>
+            <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center">
+              <li className="nav-item">
+                <a href="#about" className="nav-link px-2 text-secondary">About</a>
+              </li> 
+              <li className="nav-item">
+                <a href="#roadmap" className="nav-link px-2 text-secondary">Roadmap</a>
+              </li>
+              <li className="nav-item">
+                <a href="#team" className="nav-link px-2 text-secondary">Team</a>
+              </li>
+              <li className="nav-item">
+                <a href="#community" className="nav-link px-2 text-secondary">Community</a>
+              </li>
+            </ul>
+          </div>
+        
         </div>
         
       </header>

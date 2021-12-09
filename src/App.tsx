75910892ef6a13a60899653658e7a073c930b059
Alex Au-Yeung <ah.auyeung@gmail.com>
2021-12-09 13:51:19 +1100

    header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './assets/carbonator_logo_color.svg';
 import './App.css';
 // add bootstrap
 import 'bootstrap/dist/css/bootstrap.css';
 
+import AppHeader from './components/header.js';
+import AppFooter from './components/footer.js';
+
+
+const axios = require('axios').default;
 
 function App() {
+
+  const [ethGas, setEthGas] = useState(0);
+
+  useEffect( () => {
+    getEthGas();
+  })
+
+  const getEthGas = async () => {
+    const res = await axios.get('https://ethgasstation.info/json/ethgasAPI.json');
+    console.log("data: ", res.data);
+    setEthGas(res.data.average);
+  }
+
   return (
     <div className="App">
-      <header className="app-header bg-dark text-white">
-        <div className="container">
-          <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-            <a href="/" className="d-flex align-items-center text-decoration-none">
-                <img src={logo} className="App-logo" alt="logo" />
-            </a>
-            <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center">
-              <li className="nav-item">
-                <a href="#about" className="nav-link px-2 text-secondary">About</a>
-              </li> 
-              <li className="nav-item">
-                <a href="#roadmap" className="nav-link px-2 text-secondary">Roadmap</a>
-              </li>
-              <li className="nav-item">
-                <a href="#team" className="nav-link px-2 text-secondary">Team</a>
-              </li>
-              <li className="nav-item">
-                <a href="#community" className="nav-link px-2 text-secondary">Community</a>
-              </li>
-            </ul>
-          </div>
-        
-        </div>
-        
-      </header>
+     <AppHeader />
+     <section>
+        data: {ethGas}
+
+    </section>
+    <AppFooter />
     </div>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,7 @@
+import React, {useState} from 'react';
+
+const AppFooter = (props) => { 
+    return <div />
+}
+
+export default AppFooter;
